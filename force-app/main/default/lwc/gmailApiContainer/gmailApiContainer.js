dab966a21c534c00ee7e9c18b6c2c3dffe90abfe
Munewxar <steven.altamirano.work@gmail.com>
2023-03-06 14:59:57 +0300

    Pagination and Send Message functionality added

--- force-app/main/default/lwc/gmailApiContainer/gmailApiContainer.js
+++ force-app/main/default/lwc/gmailApiContainer/gmailApiContainer.js
@@ -1,30 +1,132 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import MessageDetailsInputModal from 'c/messageDetailsInputModal';
+
+import getTotalMessagesAmount from '@salesforce/apex/GmailPublicApiController.getTotalMessagesAmount';
 import retrieveMessages from '@salesforce/apex/GmailPublicApiController.retrieveMessages';
+import sendMessage from '@salesforce/apex/GmailPublicApiController.sendMessage';
 
 export default class GmailApiContainer extends LightningElement {
     messages = [];
     error;
+    pageNumber = 1;
+    datasetNumber = this.pageNumber - 1;
+    totalMessagesAmount = 0;
+    totalPagesAmount = 1;
+    messagesPerPageAmount = 10;
     isLoading = true;
+    isPrevButtonDisabled = true;
+    isNextButtonDisabled = true;
 
     connectedCallback() {
-        this.callRetrieveMessages();
+        this.handleInit();
+    }
+
+    handleInit() {
+        getTotalMessagesAmount()
+        .then((result) => {
+            this.totalMessagesAmount = result;
+            this.totalPagesAmount = Math.ceil(this.totalMessagesAmount / this.messagesPerPageAmount);
+            this.error = undefined;
+
+            this.callRetrieveMessages();
+        })
+        .catch((error) => {
+            this.error = error;
+            this.totalMessagesAmount = undefined;
+
+            const event = new ShowToastEvent({
+                title: 'Error',
+                message: 'There was an error loading component',
+                variant: 'error',
+            });
+            this.dispatchEvent(event);
+        })
     }
 
     callRetrieveMessages() {
-        retrieveMessages()
+        this.isLoading = true;
+        this.disableOrEnablePrevAndNexButtons();
+
+        retrieveMessages({ 'datasetNumber': this.datasetNumber })
             .then(result => {
-                console.log(result);
                 this.messages = result;
                 this.error = undefined;
             })
             .catch(error => {
                 this.error = error;
                 this.messages = undefined;
+
+                const event = new ShowToastEvent({
+                    title: 'Error',
+                    message: 'There was an error retrieving messages',
+                    variant: 'error',
+                });
+                this.dispatchEvent(event);
             })
             .finally(() => {
                 this.isLoading = false;
             })
     }
 
-    handleSendEmail() { }
+    disableOrEnablePrevAndNexButtons() {
+        if (this.totalPagesAmount > this.pageNumber) {
+            this.isNextButtonDisabled = false;
+        } else {
+            this.isNextButtonDisabled = true;
+        }
+
+        if (this.pageNumber > 1) {
+            this.isPrevButtonDisabled = false;
+        } else {
+            this.isPrevButtonDisabled = true;
+        }
+    }
+
+    handlePrevPageNav() {
+        this.pageNumber--;
+        this.datasetNumber--;
+        this.callRetrieveMessages();
+    }
+
+    handleNextPageNav() {
+        this.pageNumber++;
+        this.datasetNumber++;
+        this.callRetrieveMessages();
+    }
+
+    handleOpenMessageDetailsInputModal() {
+        MessageDetailsInputModal.open({
+            size: 'small',
+            onsendmessage: (event) => {
+                event.stopPropagation();
+                this.handleSendMessage(event.detail);
+            }
+        });
+    }
+
+    handleSendMessage(messageInfo) {
+        this.callSendMessage(messageInfo);
+    }
+
+    callSendMessage(messageInfo) {
+        sendMessage({ 'serializedMessageInfo': JSON.stringify(messageInfo) })
+            .then(() => {
+                const event = new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Message sent successfully',
+                    variant: 'success',
+                });
+                this.dispatchEvent(event);
+            })
+            .catch(() => {
+                const event = new ShowToastEvent({
+                    title: 'Error',
+                    message: 'There was an error sending message',
+                    variant: 'error',
+                });
+                this.dispatchEvent(event);
+            })
+    }
 }
--- force-app/main/default/lwc/gmailApiMessage/gmailApiMessage.js
+++ force-app/main/default/lwc/gmailApiMessage/gmailApiMessage.js
@@ -1,18 +1,21 @@
 import { api, LightningElement } from 'lwc';
+import MessageDetailsModal from 'c/messageDetailsModal';
 
 export default class GmailApiMessage extends LightningElement {
     @api message;
 
     get senderName() {
-        return this.message.fromValue;
+        return this.message.Sender_Name__c;
     }
 
     get messageText() {
-        return this.message.messageValue;
+        return this.message.Message_Text__c;
     }
 
     handleMessageClick() {
-        const showEmailEvent = new CustomEvent('showemail');
-        showEmailEvent.fire();
+        MessageDetailsModal.open({
+            size: 'small',
+            message: this.message
+        });
     }
 }
